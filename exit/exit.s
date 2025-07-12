09c9fbbba46c9566e558c004fde6272b176e524f
Ramon Moraes <ramonmoraes.foss@gmail.com>
2025-07-12 17:17:25 -0300

    fixing build pipe

--- exit/exit.s
+++ exit/exit.s
@@ -1,24 +1,26 @@
-# x86
+# x64
 .section .data
+
+yes DB 'y'  # invalid data assignment
 
 .section .text
 
 .globl _start
 
 _start:
-        mov $1, %eax
-        add $1, %eax
-        call print
+        mov $41, %rax
+        add $1, %rax
+#        call print
 
-print:
-        mov $1, %rax  # syscal write
+#print:
+        mov $1, %rax    # syscal write
         mov $1, %rdi    # fd: file descriptor - (stdout)
-        mov %eax, %rsi  # buf: bytes to print - number 2
-        mov $1, %edx  # count: length of msg - 1 byte
+        mov yes, %rsi  # buf: bytes to print - number 2
+        mov $4, %rdx    # count: length of msg - 1 byte
         syscall
-        call exit
+#        call exit
 
-exit:
+#exit:
         mov $60, %rax # syscal for exit program
         mov $0, %rdi # return code value
         syscall
